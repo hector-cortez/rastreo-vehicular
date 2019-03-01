3a70df3a574bf98c92d32b0507edb0985c1bb365
hcortez <hector.cortez.ch@gmail.com>
2019-03-01 00:26:40 -0400

    Centralizado componentes varios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { PrincipalComponent } from './principal/principal.component';
-import { MonitoreoComponent } from './monitoreo/monitoreo.component';
-import { ReportesComponent } from './reportes/reportes.component';
-import { PosicionActualComponent } from './posicion-actual/posicion-actual.component';
-import { RecorridoComponent } from './recorrido/recorrido.component';
-import { TrazadoRecorridoComponent } from './trazado-recorrido/trazado-recorrido.component';
-import { ExcesoVelocidadComponent } from './exceso-velocidad/exceso-velocidad.component';
-import { ParadasComponent } from './paradas/paradas.component';
-import { EventosComponent } from './eventos/eventos.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { MonitoreoComponent } from './components/monitoreo/monitoreo.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { PosicionActualComponent } from './components/posicion-actual/posicion-actual.component';
+import { RecorridoComponent } from './components/recorrido/recorrido.component';
+import { TrazadoRecorridoComponent } from './components/trazado-recorrido/trazado-recorrido.component';
+import { ExcesoVelocidadComponent } from './components/exceso-velocidad/exceso-velocidad.component';
+import { ParadasComponent } from './components/paradas/paradas.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
