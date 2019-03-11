e3f5eb8037ba5faf6f7d2abea0b6bdf51bc72d06
Hector Cortez <hector.cortez.ch@gmail.com>
2019-03-11 11:52:34 -0400

    Cmabios en el modulo de Excesos de velocidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ExcesoVelocidadComponent } from './components/exceso-velocidad/exceso-v
 import { ParadasComponent } from './components/paradas/paradas.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { RecorridoMostrarComponent } from './components/recorrido-mostrar/recorrido-mostrar.component';
+import { ExcesoVelocidadReporteComponent } from './components/exceso-velocidad-reporte/exceso-velocidad-reporte.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'recorrido-mostrar', component: RecorridoMostrarComponent },
   { path: 'trazado-recorrido', component: TrazadoRecorridoComponent },
   { path: 'exceso-velocidad', component: ExcesoVelocidadComponent },
+  { path: 'exceso-velocidad-reporte', component: ExcesoVelocidadReporteComponent },
   { path: 'paradas', component: ParadasComponent },
   { path: 'eventos', component: EventosComponent },
 ];
