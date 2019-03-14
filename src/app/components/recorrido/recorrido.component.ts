a20daa0bc6a80c20d81699e7c8a9047c320fceba
Hector Cortez <hector.cortez.ch@gmail.com>
2019-03-14 01:48:44 -0400

    Ajustes para paso de parametros entre componentes

--- src/app/components/recorrido/recorrido.component.ts
+++ src/app/components/recorrido/recorrido.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
 import { RouterModule, Router } from '@angular/router';
 
@@ -13,19 +13,23 @@ export interface Vehiculo {
   styleUrls: ['./recorrido.component.css']
 })
 export class RecorridoComponent implements OnInit {
-
+  @ViewChild('fDesde') fDesde: ElementRef;
+  @ViewChild('fHasta') fHasta: ElementRef;
   
   vehiculos: Vehiculo[] = [
-    {value: '1', viewValue: '2153-IXU-2153-IXU'},
-    {value: '2', viewValue: '2190-GLI-2190-GLI'},
-    {value: '3', viewValue: '2305-CBR-2305-CBR'},
-    {value: '4', viewValue: '2541-CNA-2541-CNA'},
-    {value: '5', viewValue: '2547-EST-2547-EST'},
-    {value: '6', viewValue: '2552-TTD-2552-TTD'},
-    {value: '7', viewValue: '2806-TBL-2806-TBL'},
-    {value: '8', viewValue: '2994-ZPD-2994-ZPD'},
-    {value: '9', viewValue: '251-YXX-4251-YXX'},
+    {value: '2153-IXU', viewValue: '2153-IXU-2153-IXU'},
+    {value: '2190-GLI', viewValue: '2190-GLI-2190-GLI'},
+    {value: '2305-CBR', viewValue: '2305-CBR-2305-CBR'},
+    {value: '2541-CNA', viewValue: '2541-CNA-2541-CNA'},
+    {value: '2547-EST', viewValue: '2547-EST-2547-EST'},
+    {value: '2552-TTD', viewValue: '2552-TTD-2552-TTD'},
+    {value: '2806-TBL', viewValue: '2806-TBL-2806-TBL'},
+    {value: '2994-ZPD', viewValue: '2994-ZPD-2994-ZPD'},
+    {value: '251-YXX', viewValue: '251-YXX-4251-YXX'},
   ];
+
+  selVehiculo = '';
+  // fDesde = '';
 
   constructor(private router: Router) { }
 
@@ -33,7 +37,9 @@ export class RecorridoComponent implements OnInit {
   }
  
   mostrarRecorrido(): void {
-     this.router.navigate(["recorrido-mostrar"]);
+    console.log(this.selVehiculo);
+    console.log(this.fDesde.nativeElement.value);
+    this.router.navigate(["recorrido-mostrar", this.selVehiculo, this.fDesde.nativeElement.value, this.fHasta.nativeElement.value]);
   }
 
 }
